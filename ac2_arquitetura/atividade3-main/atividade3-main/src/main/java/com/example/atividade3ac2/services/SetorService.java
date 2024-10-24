a907f0d71e66310fc9fb7445dc785f8ca01a5dc9
Vinicius Leite <112095215+ViniciusLe1te@users.noreply.github.com>
2024-10-24 20:45:32 -0300

    Delete ac2_arquitetura/atividade3-main/atividade3-main directory

--- ac2_arquitetura/atividade3-main/atividade3-main/src/main/java/com/example/atividade3ac2/services/SetorService.java
+++
@@ -1,8 +0,0 @@
-package com.example.atividade3ac2.services;
-
-import com.example.atividade3ac2.dto.SetorDTO;
-
-public interface SetorService {
-    void adicionar(SetorDTO setor);
-    SetorDTO buscarSetorId(Integer id);
-}
